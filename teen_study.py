c7e940ad0b647d7cb351ac5436f98a188df5b397
NekoRectifier <hyx123250@gmail.com>
2022-03-05 23:47:39 +0800

    试开发添加状态栏功能
    目前暂缺:
    1. 浅色/深色识别
    2. 字体自动居中
    3.(不太可能实现的)垂直位置自动校准

--- teen_study.py
+++ teen_study.py
@@ -1,16 +1,24 @@
 import requests
 import requests.utils
+import yaml
+from random import choice
 from urllib.request import urlretrieve
 import PIL.Image as Image
+import PIL.ImageDraw as Draw
+import PIL.ImageFont as Font
 import io
-import ssl
 from bs4 import BeautifulSoup
 
-openid = "XXXXXXXXXXXXXXXXXXXXXXX"
-token = "XXXXXXXXXXXXXXXXXXXXXXXX"
-info_pic_gen_server = "http://124.222.5.227:8080?course=" # 这是我服务器上提供的图片生成页面
+with open('config.yml', 'r', encoding='utf-8') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
 
-ssl._create_default_https_context = ssl._create_unverified_context
+
+def add_text(img_path, text, x=0, y=0, color=(255, 255, 255), size=40):
+    image = Image.open(img_path)
+    draw = Draw.Draw(image)
+    style = Font.truetype("font/NotoSansSC.otf", size, encoding='utf-8')
+    draw.text((x, y), text, color, font=style)
+    return image
 
 
 def show_exit(content):
@@ -33,7 +41,10 @@ def get_code(s):
         "Host": "h5.cyol.com",
         "Connection": "keep-alive",
         "Accept": "application/json, text/javascript, */*; q=0.01",
-        "User-Agent": "Mozilla/5.0 (Linux; Android 10; PACM00 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3164 MMWEBSDK/20211001 Mobile Safari/537.36 MMWEBID/556 MicroMessenger/8.0.16.2040(0x28001056) Process/toolsmp WeChat/arm32 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; PACM00 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, "
+                      "like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3164 MMWEBSDK/20211001 Mobile Safari/537.36 "
+                      "MMWEBID/556 MicroMessenger/8.0.16.2040(0x28001056) Process/toolsmp WeChat/arm32 Weixin "
+                      "NetType/WIFI Language/zh_CN ABI/arm64",
         "Origin": "http://h5.cyol.com",
         "X-Requested-With": "com.tencent.mm",
         "Sec-Fetch-Site": "cross-site",
@@ -43,10 +54,6 @@ def get_code(s):
         "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
     }
     resp = s.get(url, headers=headers).json()
-
-    print(list(resp)[-1])
-
-    # 奇怪的代码是大学习的课程代号.... 有趣
 
     return list(resp)[-1]
 
@@ -59,7 +66,10 @@ def get_user(s):
     headers = {
         "Host": "api.fjg360.cn",
         "Connection": "keep-alive",
-        "User-Agent": "Mozilla/5.0 (Linux; Android 10; PACM00 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3164 MMWEBSDK/20211001 Mobile Safari/537.36 MMWEBID/556 MicroMessenger/8.0.16.2040(0x28001056) Process/toolsmp WeChat/arm32 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; PACM00 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, "
+                      "like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3164 MMWEBSDK/20211001 Mobile Safari/537.36 "
+                      "MMWEBID/556 MicroMessenger/8.0.16.2040(0x28001056) Process/toolsmp WeChat/arm32 Weixin "
+                      "NetType/WIFI Language/zh_CN ABI/arm64",
         "Accept": "*/*",
         "X-Requested-With": "com.tencent.mm",
         "Sec-Fetch-Site": "same-site",
@@ -68,7 +78,7 @@ def get_user(s):
         "Accept-Encoding": "gzip, deflate",
         "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
     }
-    url = "https://api.fjg360.cn/index.php?m=vote&c=index&a=get_members&openid=" + openid
+    url = "https://api.fjg360.cn/index.php?m=vote&c=index&a=get_members&openid=" + config['openid']
     resp = s.get(url, headers=headers).json()
     if resp.get("code") == 1:
         print(resp.get("h5_ask_member"))
@@ -84,8 +94,12 @@ def get_course(s, code):
         "Host": "h5.cyol.com",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Linux; Android 10; PACM00 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3164 MMWEBSDK/20211001 Mobile Safari/537.36 MMWEBID/556 MicroMessenger/8.0.16.2040(0x28001056) Process/toolsmp WeChat/arm32 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/wxpic,image/tpg,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; PACM00 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, "
+                      "like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3164 MMWEBSDK/20211001 Mobile Safari/537.36 "
+                      "MMWEBID/556 MicroMessenger/8.0.16.2040(0x28001056) Process/toolsmp WeChat/arm32 Weixin "
+                      "NetType/WIFI Language/zh_CN ABI/arm64",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/wxpic,image/tpg,image/apng,"
+                  "*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "X-Requested-With": "com.tencent.mm",
         "Accept-Encoding": "gzip, deflate",
         "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -101,14 +115,20 @@ def get_course(s, code):
 def save_door(info, course, s):
     """
     调用API提交用户进入页面信息至青春湖北数据库
+    :param s:
+    :param course:
     :param info:
     :return:
     """
     headers = {
         "Host": "cp.fjg360.cn",
         "Connection": "keep-alive",
-        "Accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
-        "User-Agent": "Mozilla/5.0 (Linux; Android 10; PACM00 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3164 MMWEBSDK/20211001 Mobile Safari/537.36 MMWEBID/556 MicroMessenger/8.0.16.2040(0x28001056) Process/toolsmp WeChat/arm32 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
+        "Accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, "
+                  "*/*; q=0.01",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; PACM00 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, "
+                      "like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3164 MMWEBSDK/20211001 Mobile Safari/537.36 "
+                      "MMWEBID/556 MicroMessenger/8.0.16.2040(0x28001056) Process/toolsmp WeChat/arm32 Weixin "
+                      "NetType/WIFI Language/zh_CN ABI/arm64",
         "X-Requested-With": "XMLHttpRequest",
         "Sec-Fetch-Site": "same-origin",
         "Sec-Fetch-Mode": "cors",
@@ -123,7 +143,7 @@ def save_door(info, course, s):
     url += "&city=" + info["danwei1"]
     url += "&danwei2=" + info["danwei3"]
     url += "&danwei=" + info["danwei2"]
-    url += "&openid=" + openid
+    url += "&openid=" + config['openid']
     url += "&num=10"
     url += "&lesson_name=" + course
     resp = s.get(url, headers=headers).json()
@@ -146,34 +166,51 @@ def get_finish_pic(code):
 
     print(base_url)
 
-    urlretrieve(base_url, "/tmp/finish.jpg")
+    urlretrieve(base_url, config['output_dir'] + "finish.jpg")
 
 
 def get_user_info_pic(course, name, id, company):
     """
     获取用户信息相关截图
-    :param course, name, id, company:
+    :param id:
+    :param name:
+    :param course:
+    :param company:
     :return:
     """
-    req_url = info_pic_gen_server
-    req_url += str(course)
+    req_url = config['personal_info_generate_server']
+    req_url += "?course=" + str(course)
     req_url += "&name=" + str(name)
     req_url += "&id=" + str(id)
     req_url += "&company=" + str(company)
 
     data = {
-        'url': req_url,  
-        'token': token,  # 需替换为自己的screenshotmaster api token
+        'url': req_url,
+        'token': config['token'],  # 需替换为自己的screenshotmaster api token
         'width': '828',  # 此大小与中青在线提供的完成图片大小一致
         'height': '1366',
-        'delay': '50',
+        'delay': '1000',
         'device': 'mobile'
     }
 
     r = requests.post("https://www.screenshotmaster.com/api/v1/screenshot", data=data)
     img = Image.open(io.BytesIO(r.content))
     img = img.convert("RGB")
-    img.save("/tmp/personal_info.jpg")
+    img.save(config['output_dir'] + "personal_info.jpg")
+
+
+def image_processing(course_name):
+    print(course_name)
+    if config['add_status_bar']:
+        img = choice(config['status_images'])
+        print(img)
+
+        # TODO:检测颜色?
+
+        img = add_text(img, "“青年大学习”第十三季", 175, 93, (0, 0, 0), 40)
+        img.show()
+
+        # TODO:再次拼接图片
 
 
 def run():
@@ -185,6 +222,8 @@ def run():
     get_finish_pic(code)
     get_user_info_pic(course, user_info["name"], user_info["uid"],
                       user_info["danwei1"] + user_info["danwei2"] + user_info["danwei3"])
+
+    image_processing(course)
 
 
 if __name__ == '__main__':
